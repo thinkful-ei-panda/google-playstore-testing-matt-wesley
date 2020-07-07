6d1fb6f67d78e72038eeb593b6d4fc43ab4f239e
Wesley Rou <wesleyrou@gmail.com>
2020-07-07 09:57:59 -0700

    completed

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const morgan= require('morgan');
+const playStore= require('./playStore.js');
+
+const app = express();
+
+
+app.use(morgan('dev'));
+
+app.get('/apps', (req,res)=>{
+  const sort=req.query.sort;
+  const genres=req.query.genres;
+  let filteredApps;
+
+  if (sort && sort!=='rating' && sort!=='app'){
+    res
+      .status(400)
+      .send('Invalid sort value');
+  }
+
+  const genreOptions=['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'];
+
+  if (genres && !(genreOptions.includes(genres.toLowerCase()))){
+    res
+      .status(400)
+      .send('Invalid genres value');
+  }
+
+  if (genres){
+    filteredApps=playStore.filter(app => {
+      return app.Genres.toLocaleLowerCase()===genres.toLowerCase();
+    });
+  }else{
+    filteredApps=playStore;
+  }
+
+  if(sort==='rating'){
+    filteredApps.sort((app1,app2) =>{
+      return app2.Rating-app1.Rating;
+    });
+  }else if(sort==='app'){
+    filteredApps.sort((app1,app2)=>{
+        return app1.App.toLowerCase().charCodeAt(0) -app2.App.toLowerCase().charCodeAt(0);
+    });
+  }
+  res.json(filteredApps);
+});
+
+app.listen(8000,()=>{
+  console.log('Server 8000 is running');
+});
+
